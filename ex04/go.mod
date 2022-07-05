1dcd5c2447d7cf515ee0c8a7a49c3cd0149870f7
brucetieu <brucetieu5@gmail.com>
2022-07-05 07:08:41 -0500

    Add dfs solution

---
+++ ex04/go.mod
@@ -0,0 +1,5 @@
+module github.com/brucetieu/gophercises/ex04
+
+go 1.17
+
+require golang.org/x/net v0.0.0-20220630215102-69896b714898 // indirect
---
+++ ex04/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type Link struct {
+	Href string
+	Text string
+}
+
+func readHTML(filename string) string {
+	html, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(html)
+}
+
+func prettyFormat(data interface{}) string {
+	bytes, _ := json.MarshalIndent(data, "", "\t")
+	return string(bytes)
+}
+
+func traverseHTML(node *html.Node, links *[]Link) {
+	if node.Type == html.ElementNode && node.Data == "a" {
+		href := ""
+		for _, a := range node.Attr {
+			if a.Key == "href" {
+				href = a.Val
+				break
+			}
+		}
+
+		atagText := traverseTag(node, "")
+		if href != "" {
+			link := Link{
+				Href: href,
+				Text: atagText,
+			}
+			*links = append(*links, link)
+		}
+	}
+
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		traverseHTML(c, links)
+	}
+}
+
+func htmlParser(htmlString string) []Link{
+	doc, err := html.Parse(strings.NewReader(htmlString))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	links := make([]Link, 0)
+
+	traverseHTML(doc, &links)
+	
+	return links
+}
+
+func traverseTag(node *html.Node, s string) string {
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.TextNode {
+			s += strings.TrimSpace(c.Data) + " "
+			
+		}
+		s = traverseTag(c, s)
+	}
+
+	return strings.TrimSpace(s)
+}
+
+func main() {
+	filePtr := flag.String("htmlfile", "", "HTML file to parse")
+
+	flag.Parse()
+
+	htmlString := readHTML(*filePtr)
+	links := htmlParser(htmlString)
+
+	fmt.Println("links: ", prettyFormat(links))
+}
